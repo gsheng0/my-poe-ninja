1ba47907154cdd7553712e66f04c1e939dffbe67
George Sheng <gsheng@georges-mbp.myfiosgateway.com>
2023-12-18 04:14:18 -0500

    Added constants file

---
+++ server/config/constants.js
@@ -0,0 +1,12 @@
+const CATEGORY_NAMES = 
+    ["Currency", "Tattoo", "Omen", "Fragment", "Oil", "Incubator", 
+    "Scarab", "Fossil", "Resonator", "Essence", "DivinationCard", 
+    "SkillGem", "UniqueArmour", "UniqueAccessory"];
+const POE_NINJA_BASE_URL = "https://poe.ninja/api/data/";
+const CURRENT_LEAGUE = "Ancestor";
+
+module.exports = {
+    CATEGORY_NAMES,
+    POE_NINJA_BASE_URL,
+    CURRENT_LEAGUE
+}
