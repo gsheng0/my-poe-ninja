7e491be545c23bcdb18e5c31977af3dc8fe413d8
George Sheng <gsheng@georges-mbp.myfiosgateway.com>
2023-12-18 03:56:03 -0500

    started working on data retrieval from poe ninja api

--- server/data/ninja.js
+++ server/data/ninja.js
@@ -1,1 +1,12 @@
-const baseUrl = "https://poe.ninja/api/data/currencyoverview?league=Blight&type=Currency"
+const currencyUrl = "https://poe.ninja/api/data/currencyoverview?league=Affliction&type=Currency"
+
+const getCurrencyData = async() => {
+    return (await (await fetch(currencyUrl)).json()).lines;
+}
+
+const main = async() => {
+    const data = await getCurrencyData();
+
+    console.log(data);
+}
+main();
